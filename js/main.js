c4d6b89b4e1709e78aefcc40e1f42ee07dab3b76
525tanya <123821489+525tanya@users.noreply.github.com>
2023-04-08 18:37:00 +0300

    calc

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 let num_1, num_2, operation, result;
-num_1 = +prompt('Введите первое число',+num_1);
-num_2 = +prompt('Введите второе число',+num_2);
+num_1 = +prompt('Введите первое число',num_1);
+num_2 = +prompt('Введите второе число',num_2);
 operation = prompt('Введите математическую операцию ( +, -,*,/)', '+');
 if (operation === '+') result = num_1 + num_2;
 
